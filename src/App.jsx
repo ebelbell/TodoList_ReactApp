8a8911f19ebb1c6193bcffa2f6e6ca4f3ea75af0
E Abella <eabella@Es-MacBook-Air.local>
2024-07-15 09:17:30 -0700

    exported the ACTIONS and created the Delete Button

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useReducer, useState } from 'react';
 import './App.css';
 import Todo from './components/Todo';
 
-const ACTIONS = {
+export const ACTIONS = {
   ADD_TODO: 'add-todo', //add a new task 
   TOGGLE_TODO: 'toggle-todo', //completing/ mark off a task
   EDIT_TODO: 'edit-todo', //edit a task
@@ -15,12 +15,18 @@ function reducer(todos, action) { //define the
   // change it so that the ACTIONS.ADD_TODO have something to do. start with switch statements  
   switch (action.type) {
     case ACTIONS.ADD_TODO:
+      return [...todos, newTask(action.payload.taskEntry)];
+    case ACTIONS.TOGGLE_TODO:
       return todos.map(todo => {
         if (todo.id === action.payload.id) {
           return { ...todo, complete: !todo.complete } // new todo that is complete
         }
         return todo;
       })
+    case ACTIONS.DELETE_TODO:
+      return todos.filter(todo => todo.id !== action.payload.id) // filter to remove the unwanted todos. if the todo id is NOT = payload id, then it stays on the list, otherwise, it gets removed
+    default:
+      return todos;
   }
 }
 
@@ -52,6 +58,8 @@ const App = () => {
         {/* placeholder='Add task' */}
         <input type='text' value={taskEntry} placeholder='Add task' onChange={e => setTaskEntry(e.target.value)} />
         <button> Add </button>
+        <button> Delete </button>
+
       </form>
       {todos.map(todo => {
         return <Todo key = {todo.id} todo={todo} dispatch={dispatch}/>
