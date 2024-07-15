e482be48347f0dfdab686afb8e435ff7f344bbdb
E Abella <eabella@Es-MacBook-Air.local>
2024-07-15 05:00:15 -0700

    added a form entry to addthe Add Task button

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
-import { useState } from 'react'
+// imports 
+import { useReducer, useState } from 'react';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0);
+// reducer function so that it updates and returns state
+function reducer(state, action) { //define the 
+  
+}
+
+
+
+const App = () => {
+  const [todos, dispatch] = useReducer(reducer, []);
+  const [taskEntry, setTaskEntry] = useState('');// call the state. change the tasks every user entry
+
+
+
 
   return (
     <>
       <h1>Todo List</h1>
-
-      {/* A list of "todo" items, which are strings listing activities
-      to be accomplished (e.g. "find that missing sock") */}
+      
+      {/* add a user form for task entry */}
+      <form>
+        <input type='text' value={taskEntry} onChange={e => setTaskEntry(e.target.value)} />
+      </form>
     </>
-  );
-};
+      
+  )
+}
+
 
 export default App;
